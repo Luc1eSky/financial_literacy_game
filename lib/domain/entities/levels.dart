a5a6123ddaa90085cf8d9aea2bd09b534bd4a5e7
Niklas <beck.niklas@gmail.com>
2023-04-25 11:34:09 -0700

    add level 2 back in

--- lib/domain/entities/levels.dart
+++ lib/domain/entities/levels.dart
@@ -15,20 +15,20 @@ List<Level> levels = [
     savingsRate: 0.00,
   ),
   // Level 2 - cash only, no interest on cash, low starting cash amount
-  // Level(
-  //   startingCash: 15,
-  //   cashGoal: 50,
-  //   assets: [chickens.copyWith(riskLevel: 0)],
-  //   loan: Loan(interestRate: 0.25, asset: cow),
-  //   assetTypeRandomized: true,
-  //   assetIncomeAndCostsRandomized: true,
-  //   showCashBuyOption: true,
-  //   savingsRate: 0.00,
-  // ),
+  Level(
+    startingCash: 15,
+    cashGoal: 50,
+    assets: [chickens.copyWith(riskLevel: 0)],
+    loan: Loan(interestRate: 0.25, asset: cow),
+    assetTypeRandomized: true,
+    assetIncomeAndCostsRandomized: true,
+    showCashBuyOption: true,
+    savingsRate: 0.00,
+  ),
   // Level 3 - borrow only, no interest on cash, low starting cash amount
   Level(
     startingCash: 15,
-    cashGoal: 30,
+    cashGoal: 50,
     assets: [chickens.copyWith(riskLevel: 0)],
     loan: Loan(interestRate: 0.20, asset: cow),
     assetTypeRandomized: true,
