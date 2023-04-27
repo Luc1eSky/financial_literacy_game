2ddd1ce77210501d6ac5947962767b5fe944321a
Lucie Schulz <schulz.lucie@gmail.com>
2023-04-26 22:25:50 -0700

    color updates

--- lib/config/color_palette.dart
+++ lib/config/color_palette.dart
@@ -4,7 +4,7 @@ class ColorPalette {
   Color get backgroundColor => const Color.fromRGBO(225, 251, 255, 1.0);
   Color get backgroundSectionCard => const Color.fromRGBO(105, 194, 207, 1.0);
   Color get backgroundContentCard => const Color.fromRGBO(61, 153, 166, 1.0);
-  Color get cashIndicator => const Color.fromRGBO(224, 64, 251, 1.0);
+  Color get cashIndicator => const Color.fromRGBO(255, 160, 122, 1.0);
   Color get popUpBackground => const Color.fromRGBO(225, 251, 255, 1.0);
   Color get buttonBackground => const Color.fromRGBO(61, 153, 166, 1.0);
   Color get buttonForeground => const Color.fromRGBO(225, 251, 255, 1.0);
